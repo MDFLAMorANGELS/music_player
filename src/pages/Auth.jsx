3981ccfcf8ef8fc84442427cda5992b1fc26cc92
valentin <78280val@gmail.com>
2024-12-26 17:31:21 +0100

    Avancement page Auth

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -6,7 +6,7 @@ function Auth() {
   const [, setToken] = useState(localStorage.getItem("token"));
 
   const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
-  const REDIRECT_URI = "https://music-player-taupe-rho.vercel.app";  // http://localhost:5173/
+  const REDIRECT_URI = "https://music-player-taupe-rho.vercel.app"; //http://localhost:5173/
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
   const RESPONSE_TYPE = "token";
   const SCOPES = ["streaming", "user-read-email", "user-read-private"];
@@ -35,10 +35,30 @@ function Auth() {
   }, []);
 
   return (
-    <div className="auth ">
-      <Button className="text-lg font-semibold" size="lg" color="success" onClick={loginSpotify}>Connect to Spotify <FaSpotify className="size-12" /></Button>
-      <p className="text-red-500 font-semibold opacity-80">*Application nécessite un compte spotify prenium</p>
-    </div>
+    <section className="flex h-[100vh] justify-around items-center flex-col ml-[-70px] md:m-0">
+      <h1 className="text-3xl text-gray-200 p-2">Music Player by Mdflamorangels❤️</h1>
+      <div className="flex flex-col items-center">
+        <Button className="text-lg font-semibold w-2/3" size="lg" color="success" onClick={loginSpotify}>Connect to Spotify <FaSpotify className="size-12" /></Button>
+        <p className="text-red-500 font-semibold opacity-100">*Application nécessite un compte spotify prenium</p>
+      </div>
+      <div className="text-white text-lg font-semibold bg-red-950 p-5 presentation md:mr-[70px] xl:mr-0 xl:w-2/3 rounded-xl shadow-sm">
+        <p className="text-xl mb-3">🎵 Présentation de l'application <span>Music Player</span> 🎵 </p>
+        <p className="">Découvrez notre nouvelle application web, conçue pour enrichir votre expérience musicale en utilisant la puissance de l'API Spotify et son Player SDK intégré ! Avec cette application, vous pouvez :</p>
+        <ul className="list-disc pl-5 my-3">
+          <li>Diffuser vos playlists favorites directement depuis Spotify.</li>
+          <li>Naviguer facilement entre vos albums, artistes et morceaux préférés.</li>
+          <li>Profiter d'une interface intuitive et élégante, adaptée à vos besoins.</li>
+        </ul>
+        <p className=" mb-3">Cependant, cette application est exclusivement disponible pour les utilisateurs disposant d'un abonnement Spotify Premium. En effet, l'accès au Player SDK de Spotify nécessite cette condition pour garantir une expérience fluide et sans interruption.</p>
+        <p className="text-xl mb-3">🔒 Pourquoi Spotify Premium ?</p>
+        <p>L'abonnement Premium permet de bénéficier des fonctionnalités avancées de l'application, notamment :</p>
+        <ul className="list-disc pl-5 my-3">
+          <li>L'absence de publicités.</li>
+          <li>La lecture en haute qualité.</li>
+          <li>Le contrôle total sur vos morceaux, y compris les sauts illimités.</li>
+        </ul>
+      </div>
+    </section>
   );
 }
 
